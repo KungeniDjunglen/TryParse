0a1b599bcf8bab33425a7b7056f83d19c8c22ced
Knugen <thhe05001@utb.vaxjo.se>
2024-01-25 11:11:40 +0100

    Gjort allt

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        int result;
+        if(!int.TryParse(Console.ReadLine(), out result)){
+            Console.WriteLine("Skriv en nuffra");
+        }
+        else{
+            Console.Write(result);
+        }
+    }
+    public static bool TryParse(string s, out int result)
+    {
+        result = int.Parse(s);
+        if(result != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
